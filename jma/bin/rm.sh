3789a3afc7b5cf6e50e3bd7497b6ca7fb22d7fc1
noyuno <noyuno@users.noreply.github.com>
2017-10-15 21:15:36 +0900

    Update

--- jma/bin/rm.sh
+++ jma/bin/rm.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -e
 
 log=/var/log/jmarm.log
+err=/tmp/jmarm.err
 data=/var/www/html/jma/data
-(
-    /home/noyuno/dotfiles/bin/now
-    du -h $data
-    find $data -ctime +3 -print -exec rm -f {} \; | wc -l
-    du -h $data
-) 1>>$log 2>&1
+now=$(/home/noyuno/dotfiles/bin/now)
+
+before=$(du -h $data | cut -f 1)
+count=$(find $data -ctime +3 -print -exec rm -f {} \; 2>$err | wc -l)
+ret=$?
+after=$(du -h $data | cut -f 1)
+
+echo "$now: $before -> [$count]($ret) -> $after" >>$log
+cat $err >>$log
 
