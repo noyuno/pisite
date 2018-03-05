e00a6de4633cdda7bc5bb5b9b5fa8a02a3f3aedb
noyuno <noyuno@users.noreply.github.com>
2018-03-05 17:15:38 +0900

    Update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cgi = "/cgi/chromecast";
 Number.prototype.padLeft = function(base, chr){
      var  len = (String(base || 10).length - String(this).length)+1;
      return len > 0? new Array(len).join(chr || '0')+this : this;
- }
+}
 
 var disk = function () {
     google.charts.load('current', {packages: ['corechart']});
